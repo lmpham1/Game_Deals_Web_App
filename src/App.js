89d7a04894896ed62ad6d09be366f03ae76c34a7
Bennet Ngan <bennet.ngan@gmail.com>
2020-10-13 15:26:39 -0400

    Added navbar with some navigation routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { Route, Switch, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Routing setup
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Navbar className="navbar navbar-default" />
+        <hr />
+
+        <h1>Find the best game deals</h1>
+
+        <hr />
+
+        <Switch>
+          <Route exact path='/' render={() => (<Home />)} />
+          <Route exact path='/about' render={() => (<About />)} />
+          <Route exact path='/login' render={() => (<Login />)} />
+          <Route exact path='/game' render={() => (<Game />)} />
+        </Switch>
+      </div>
+    )
+  }
 }
+
+//Navigation bar links
+const Navbar = () => {
+  return (
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="/">Game Deals</a>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <Link to='/'>Home</Link>
+          </li>
+          <li class="nav-item">
+            <Link to='/game'>Game</Link>
+          </li>
+          <li class="nav-item">
+            <Link to='about'>About</Link>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <ul class="navbar-nav navbar-right">
+          <li class="nav-item">
+            <Link to='/login'>
+              <button type="button" class="btn btn-success">Login</button>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+  )
+}
+
+//Home Component
+const Home = () => {
+  return (
+
+    <div class="md-form active-purple active-purple-2 mb-3">
+      <input class="form-control" type="text" placeholder="Search for a game title" aria-label="Search"/>
+    </div>
+
+  )
+}
+
+//About component
+const About = () => {
+  return (
+    <div>
+      <p>This website was created by Group 8</p>
+    </div>
+  )
+}
+
+//Login Component
+const Login = () => {
+  return (
+    <div>
+      <p>Login Page</p>
+    </div>
+  )
+}
+
+//Game Component
+const Game = () => {
+  return (
+    <div>
+      <p>Game</p>
+    </div>
+  )
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter} from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter foo="bar">
+      <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
