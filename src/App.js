550ab68cc1be4ce196a6b7857f090374e2f4e836
Minh Pham <est2000vn@gmail.com>
2020-10-14 17:29:30 -0400

    Added GameSearch component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { Component } from 'react';
 import { Route, Switch, Link } from "react-router-dom";
+import GameSearch from './GameSearch';
 
 //Routing setup
 class App extends Component {
@@ -18,7 +19,6 @@ class App extends Component {
           <Route exact path='/' render={() => (<Home />)} />
           <Route exact path='/about' render={() => (<About />)} />
           <Route exact path='/login' render={() => (<Login />)} />
-          <Route exact path='/game' render={() => (<Game />)} />
         </Switch>
       </div>
     )
@@ -29,28 +29,25 @@ class App extends Component {
 const Navbar = () => {
   return (
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="/">Game Deals</a>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">Game Deals</a>
 
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
             <Link to='/'>Home</Link>
           </li>
-          <li class="nav-item">
-            <Link to='/game'>Game</Link>
-          </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <Link to='about'>About</Link>
           </li>
         </ul>
       </div>
 
       <div>
-        <ul class="navbar-nav navbar-right">
-          <li class="nav-item">
+        <ul className="navbar-nav navbar-right">
+          <li className="nav-item">
             <Link to='/login'>
-              <button type="button" class="btn btn-success">Login</button>
+              <button type="button" className="btn btn-success">Login</button>
             </Link>
           </li>
         </ul>
@@ -62,14 +59,18 @@ const Navbar = () => {
 
 //Home Component
 const Home = () => {
+
+  var gameName = "";
   return (
 
-    <div class="md-form active-purple active-purple-2 mb-3">
-      <input class="form-control" type="text" placeholder="Search for a game title" aria-label="Search"/>
+    <div className="md-form active-purple active-purple-2 mb-3">
+      <GameSearch></GameSearch>
     </div>
 
   )
 }
+
+
 
 //About component
 const About = () => {
